678959f55cc96c6a8ee4dcc6ad38473018aae9e4
Jason <jason@kayoway.com>
2017-07-04 23:46:34 +0900

    first commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='hangouts_client',
+    version='0.1.0',
+    description='Class for sending/receiving messages using Hangouts',
+    long_description=long_description,
+    url='https://github.com/ammgws/hangouts_client',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3.6',
+    ],
+    python_requires='>=3.6',
+    py_modules=['hangoutsclient'],
+    install_requires=['sleekxmpp'],
+    dependency_links=['https://github.com/ammgws/google_auth/tarball/master']
+)
